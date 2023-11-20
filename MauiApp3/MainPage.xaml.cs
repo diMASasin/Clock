3fd94fa9175f133af870727fe9eee9944a92c5e4
diMASasin <35368058+diMASasin@users.noreply.github.com>
2023-11-20 17:56:13 +0500

    Refactoring

--- MauiApp3/MainPage.xaml.cs
+++ MauiApp3/MainPage.xaml.cs
@@ -2,23 +2,16 @@
 {
     public partial class MainPage : ContentPage
     {
-        SevenSegmentIndicator _indicator1;
-        SevenSegmentIndicator _indicator2;
-        SevenSegmentIndicator _indicator3;
-        SevenSegmentIndicator _indicator4;
-        SevenSegmentIndicator _indicator5;
-        SevenSegmentIndicator _indicator6;
+        private readonly SevenSegmentIndicator _indicator1 = new();
+        private readonly SevenSegmentIndicator _indicator2 = new();
+        private readonly SevenSegmentIndicator _indicator3 = new();
+        private readonly SevenSegmentIndicator _indicator4 = new();
+        private readonly SevenSegmentIndicator _indicator5 = new();
+        private readonly SevenSegmentIndicator _indicator6 = new();
 
         public MainPage()
         {
             InitializeComponent();
-            _indicator1 = new(Segment000, Segment010, Segment011, Segment020, Segment030, Segment031, Segment040);
-            _indicator2 = new(Segment100, Segment110, Segment111, Segment120, Segment130, Segment131, Segment140);
-            _indicator3 = new(Segment200, Segment210, Segment211, Segment220, Segment230, Segment231, Segment240);
-            _indicator4 = new(Segment300, Segment310, Segment311, Segment320, Segment330, Segment331, Segment340);
-            _indicator5 = new(Segment400, Segment410, Segment411, Segment420, Segment430, Segment431, Segment440);
-            _indicator6 = new(Segment500, Segment510, Segment511, Segment520, Segment530, Segment531, Segment540);
-
             StartTimer();
         }
 
@@ -33,13 +26,24 @@
 
                 var now = DateTime.Now;
 
-                _indicator1.ChangeSegmentsVisibilityByNumber( now.Hour / 10);
+                _indicator1.ChangeSegmentsVisibilityByNumber(now.Hour / 10);
                 _indicator2.ChangeSegmentsVisibilityByNumber(now.Hour % 10);
                 _indicator3.ChangeSegmentsVisibilityByNumber(now.Minute / 10);
                 _indicator4.ChangeSegmentsVisibilityByNumber(now.Minute % 10);
                 _indicator5.ChangeSegmentsVisibilityByNumber(now.Second / 10);
                 _indicator6.ChangeSegmentsVisibilityByNumber(now.Second % 10);
+                UpdateAllSevenSegmentIndicators();
             }
+        }
+
+        private void UpdateAllSevenSegmentIndicators()
+        {
+            _indicator1.UpdateSegments(Segment000, Segment010, Segment011, Segment020, Segment030, Segment031, Segment040);
+            _indicator2.UpdateSegments(Segment100, Segment110, Segment111, Segment120, Segment130, Segment131, Segment140);
+            _indicator3.UpdateSegments(Segment200, Segment210, Segment211, Segment220, Segment230, Segment231, Segment240);
+            _indicator4.UpdateSegments(Segment300, Segment310, Segment311, Segment320, Segment330, Segment331, Segment340);
+            _indicator5.UpdateSegments(Segment400, Segment410, Segment411, Segment420, Segment430, Segment431, Segment440);
+            _indicator6.UpdateSegments(Segment500, Segment510, Segment511, Segment520, Segment530, Segment531, Segment540);
         }
     }
 }
--- MauiApp3/SevenSegmentIndicator.cs
+++ MauiApp3/SevenSegmentIndicator.cs
@@ -1,26 +1,14 @@
 ﻿namespace MauiApp3
 {
-    public class SevenSegmentIndicator
+    internal class SevenSegmentIndicator
     {
-        private readonly BoxView _topSegment;
-        private readonly BoxView _leftTopSegment;
-        private readonly BoxView _rightTopSegment;
-        private readonly BoxView _middleSegment;
-        private readonly BoxView _leftBottomSegment;
-        private readonly BoxView _rightBottomSegment;
-        private readonly BoxView _bottomSegment;
-
-        public SevenSegmentIndicator(BoxView topSegment, BoxView leftTopSegment, BoxView RightTopSegment, BoxView middleSegment,
-            BoxView leftBottomSegment, BoxView rightBottomSegment, BoxView bottomSegment)
-        {
-            _topSegment = topSegment;
-            _leftTopSegment = leftTopSegment;
-            _rightTopSegment = RightTopSegment;
-            _middleSegment = middleSegment;
-            _leftBottomSegment = leftBottomSegment;
-            _rightBottomSegment = rightBottomSegment;
-            _bottomSegment = bottomSegment;
-        }
+        public bool TopSegment { get; private set; }
+        public bool LeftTopSegment { get; private set; }
+        public bool RightTopSegment { get; private set; }
+        public bool MiddleSegment { get; private set; }
+        public bool LeftBottomSegment { get; private set; }
+        public bool RightBottomSegment { get; private set; }
+        public bool BottomSegment { get; private set; }
 
         public void ChangeSegmentsVisibilityByNumber(int digit)
         {
@@ -32,94 +20,94 @@
                 default:
                     break;
                 case 0:
-                    _topSegment.IsVisible = true;
-                    _leftTopSegment.IsVisible = true;
-                    _rightTopSegment.IsVisible = true;
-                    _middleSegment.IsVisible = false;
-                    _leftBottomSegment.IsVisible = true;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = true;
+                    TopSegment = true;
+                    LeftTopSegment = true;
+                    RightTopSegment = true;
+                    MiddleSegment = false;
+                    LeftBottomSegment = true;
+                    RightBottomSegment = true;
+                    BottomSegment = true;
                     break;
                 case 1:
-                    _topSegment.IsVisible = false;
-                    _leftTopSegment.IsVisible = false;
-                    _rightTopSegment.IsVisible = true;
-                    _middleSegment.IsVisible = false;
-                    _leftBottomSegment.IsVisible = false;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = false;
+                    TopSegment = false;
+                    LeftTopSegment = false;
+                    RightTopSegment = true;
+                    MiddleSegment = false;
+                    LeftBottomSegment = false;
+                    RightBottomSegment = true;
+                    BottomSegment = false;
                     break;
                 case 2:
-                    _topSegment.IsVisible = true;
-                    _leftTopSegment.IsVisible = false;
-                    _rightTopSegment.IsVisible = true;
-                    _middleSegment.IsVisible = true;
-                    _leftBottomSegment.IsVisible = true;
-                    _rightBottomSegment.IsVisible = false;
-                    _bottomSegment.IsVisible = true;
+                    TopSegment = true;
+                    LeftTopSegment = false;
+                    RightTopSegment = true;
+                    MiddleSegment = true;
+                    LeftBottomSegment = true;
+                    RightBottomSegment = false;
+                    BottomSegment = true;
                     break;
                 case 3:
-                    _topSegment.IsVisible = true;
-                    _leftTopSegment.IsVisible = false;
-                    _rightTopSegment.IsVisible = true;
-                    _middleSegment.IsVisible = true;
-                    _leftBottomSegment.IsVisible = false;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = true;
+                    TopSegment = true;
+                    LeftTopSegment = false;
+                    RightTopSegment = true;
+                    MiddleSegment = true;
+                    LeftBottomSegment = false;
+                    RightBottomSegment = true;
+                    BottomSegment = true;
                     break;
                 case 4:
-                    _topSegment.IsVisible = false;
-                    _leftTopSegment.IsVisible = true;
-                    _rightTopSegment.IsVisible = true;
-                    _middleSegment.IsVisible = true;
-                    _leftBottomSegment.IsVisible = false;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = false;
+                    TopSegment = false;
+                    LeftTopSegment = true;
+                    RightTopSegment = true;
+                    MiddleSegment = true;
+                    LeftBottomSegment = false;
+                    RightBottomSegment = true;
+                    BottomSegment = false;
                     break;
                 case 5:
-                    _topSegment.IsVisible = true;
-                    _leftTopSegment.IsVisible = true;
-                    _rightTopSegment.IsVisible = false;
-                    _middleSegment.IsVisible = true;
-                    _leftBottomSegment.IsVisible = false;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = true;
+                    TopSegment = true;
+                    LeftTopSegment = true;
+                    RightTopSegment = false;
+                    MiddleSegment = true;
+                    LeftBottomSegment = false;
+                    RightBottomSegment = true;
+                    BottomSegment = true;
                     break;
                 case 6:
-                    _topSegment.IsVisible = true;
-                    _leftTopSegment.IsVisible = true;
-                    _rightTopSegment.IsVisible = false;
-                    _middleSegment.IsVisible = true;
-                    _leftBottomSegment.IsVisible = true;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = true;
+                    TopSegment = true;
+                    LeftTopSegment = true;
+                    RightTopSegment = false;
+                    MiddleSegment = true;
+                    LeftBottomSegment = true;
+                    RightBottomSegment = true;
+                    BottomSegment = true;
                     break;
                 case 7:
-                    _topSegment.IsVisible = true;
-                    _leftTopSegment.IsVisible = false;
-                    _rightTopSegment.IsVisible = true;
-                    _middleSegment.IsVisible = false;
-                    _leftBottomSegment.IsVisible = false;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = false;
+                    TopSegment = true;
+                    LeftTopSegment = false;
+                    RightTopSegment = true;
+                    MiddleSegment = false;
+                    LeftBottomSegment = false;
+                    RightBottomSegment = true;
+                    BottomSegment = false;
                     break;
                 case 8:
-                    _topSegment.IsVisible = true;
-                    _leftTopSegment.IsVisible = true;
-                    _rightTopSegment.IsVisible = true;
-                    _middleSegment.IsVisible = true;
-                    _leftBottomSegment.IsVisible = true;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = true;
+                    TopSegment = true;
+                    LeftTopSegment = true;
+                    RightTopSegment = true;
+                    MiddleSegment = true;
+                    LeftBottomSegment = true;
+                    RightBottomSegment = true;
+                    BottomSegment = true;
                     break;
                 case 9:
-                    _topSegment.IsVisible = true;
-                    _leftTopSegment.IsVisible = true;
-                    _rightTopSegment.IsVisible = true;
-                    _middleSegment.IsVisible = true;
-                    _leftBottomSegment.IsVisible = false;
-                    _rightBottomSegment.IsVisible = true;
-                    _bottomSegment.IsVisible = true;
+                    TopSegment = true;
+                    LeftTopSegment = true;
+                    RightTopSegment = true;
+                    MiddleSegment = true;
+                    LeftBottomSegment = false;
+                    RightBottomSegment = true;
+                    BottomSegment = true;
                     break;
             }
         }
